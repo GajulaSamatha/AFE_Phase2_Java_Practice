fa144213e4dacbc87bfed5f39883fe2588b1818a
Samatha Gajula <samathagajulaofficial@gmail.com>
2025-07-12 09:55:44 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,23 +1,20 @@
 import java.util.Scanner;
 class Test{
     public static void main(String[] args) {
-        // Scanner sc=new Scanner(System.in);
-        // System.out.println("Enter your email address:");
-        // String email=sc.nextLine().trim();
-        // if(email.contains("@")){
-        //     int indOfAt = email.indexOf('@');
-        // String domain= email.substring(indOfAt+1);
-        // System.out.println("Domain of the email is: " + domain);
-        // }
-        // else{
-        //     System.out.println("Invalid Email Address!!");
-        // }
+        //to fetch domain of the givem email address
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your email address:");
+        String email=sc.nextLine().trim();
+        if(email.contains("@")){
+            int indOfAt = email.indexOf('@');
+            String domain= email.substring(indOfAt+1);
+            System.out.println("Domain of the email is: " + domain);
+        }
+        else{
+            System.out.println("Invalid Email Address!!");
+        }
         
-        // sc.close();
-
-       import java.util.Scanner;
-class Main {
-    public static void main(String[] args) {
+        sc.close();
         for(int i=0;i<5;i++){
             for(int j=0;j<4-i;j++){
                 System.out.print(" ");
