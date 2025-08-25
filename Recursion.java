14edf48eaedcc4f5a4568b9a820edee44b3f16fa
Samatha Gajula <samathagajulaofficial@gmail.com>
2025-08-25 16:25:06 +0000

    more practiced on recursion

--- Recursion.java
+++ Recursion.java
@@ -27,11 +27,35 @@ public class Recursion {
         int fact = factorial(n-1)*n;
         return fact;
     }
+    //display the elements of an array using recursion
+    public static void printArrRec(int[] arr,int ind){
+        if(ind == -1){
+            return;
+        }
+        System.out.println(arr[ind]);
+        printArrRec(arr,ind-1);
+    }
+    //find largest element using recursion
+    public static int findLargeRec(int arr[], int ind, int temp){
+        if(ind == -1){
+            return arr[temp];
+        }
+        if(arr[temp]<arr[ind]){
+            temp=ind;
+        }
+        return findLargeRec(arr, ind-1, temp);
+    }
     public static void main(String[] args) {
         
         Scanner sc=new Scanner(System.in);
         //printNto1Ite(sc);
-        int fact = factorial(5);
-        System.out.println("Factorial : "+fact);
+        // int fact = factorial(5);
+        // System.out.println("Factorial : "+fact);
+        
+        int arr[] = {1,2,7,4,5,6};
+        //printArrRec(arr,arr.length-1);
+        System.out.println(findLargeRec(arr,arr.length-1,arr.length-1));
+        
+        sc.close();
     }
 }
